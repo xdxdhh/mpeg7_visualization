6497c327b487dd32bad56718d6d5a21992ef9b28
xdxdhh <d.varsikova@email.cz>
2025-02-10 16:23:22 +0100

    fix address

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://mpeg7.diana.lumias.cz/api",
   headers: {
     "Content-Type": "application/json",
   },
